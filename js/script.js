550752e69eaf73d6e0f273bbcc92966953606724
MikiyasET <mikiyaslemlemu@gmail.com>
2021-02-22 11:47:46 +0300

    slider added at the home page

---
+++ js/script.js
@@ -0,0 +1,18 @@
+	var swiper = new Swiper('.swiper-container', {
+		spaceBetween: 30,
+		loop: true,
+	    speed: 2000,
+	    effect: 'fade',
+	    fadeEffect: {
+	      crossFade: true
+	    },
+	    autoplay: {
+	        delay: 4000,
+	        disableOnInteraction: false,
+	    },
+	    // Navigation arrows
+	    navigation: {
+	      nextEl: '.swiper-button-next',
+	      prevEl: '.swiper-button-prev',
+	    },
+    });
